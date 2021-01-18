32852e829dbb280f35130854dba0cc118ea65e53
Vivek <vukondpalle@gmail.com>
2021-01-18 12:31:26 +0530

    Added MongoDB database

--- app.js
+++ app.js
@@ -1,82 +1,98 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const  _ = require("lodash");
+const mongoose = require('mongoose');
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(bodyparser.urlencoded({ extended: true }));
+mongoose.connect('mongodb://localhost:27017/articles', {useNewUrlParser: true, useUnifiedTopology: true});
 
-let articles = [];
+const articleSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  content: String,
+  date: Date
+});
+
+const Article = mongoose.model('Article', articleSchema);
 
 app.get("/", function (req, res) {
-  res.render("index", {articles:articles});
+
+  Article.find({}, function(err, articles){
+    res.render("index", {articles:articles});
+  });
+
 });
 
 app.get("/newArticle", function(req,res){
      res.render("new", {title:"", description:"", content:""});
 });
 
-app.post("/edit/:articleName", function(req,res){
+app.post("/edit/:articleID", function(req,res){
 
-  let articleName = req.params.articleName;
+  let articleID = req.params.articleID;
 
-  articles.forEach((article) => {
+  Article.findById(articleID,function(err,article){
+    if(!err){
 
-    if(articleName === article.title){
-      article.title = req.body.title;
-      article.description = req.body.description;
-      article.content = req.body.content;
+        article.title = req.body.title;
+        article.description = req.body.description;
+        article.content = req.body.content;
 
-      res.redirect("/");
+        article.save();
+        res.redirect("/");
+    }else{
+      console.log(err);
     }
   })
+
 })
 
-app.get("/edit/:articleName", function(req,res){
+app.get("/edit/:articleID", function(req,res){
 
-  let articleName = req.params.articleName;
+  let articleID = req.params.articleID;
 
-  articles.forEach((article) => {
-    if(articleName === article.title){
-      res.render("edit", {title:article.title, description:article.description, content:article.content});
-    }
-  })
+  Article.findById(articleID, function(err, article){
+    res.render("edit", {title:article.title, description:article.description, content:article.content, id:article._id});
+  });
 
 });
 
-app.get("/articles/:articleName", function(req,res){
+app.get("/articles/:articleID", function(req,res){
     
-  let articleName = req.params.articleName;
+  let articleID = req.params.articleID;
+  console.log(articleID);
 
-  articles.forEach((article) => {
-    if(articleName === article.title){
-      res.render("post", {article: article});
-    };
-  })
+  Article.findById(articleID, function(err, article){
+    res.render("post", {article: article});
+    
+  });
 
 });
 
 app.post("/newArticle", function(req,res){
 
-    let article = {
-        title: req.body.title,
-        date: new Date().toLocaleDateString(),
-        description: req.body.description,
-        content: req.body.content
-    };
 
-    articles.push(article);
+  const article = new Article(
+      { 
+         title: req.body.title,
+         date: new Date().toLocaleDateString(),
+         description: req.body.description,
+         content: req.body.content 
+     }
+  );
 
-    res.redirect("/");
+  article.save();
+  // console.log(article);
+  res.redirect("/");
 });
 
 app.post("/delete", function(req,res){
   let deleteBlog = req.body.deleteName;
 
-  articles.forEach((article) => {
-    if(article.title === deleteBlog){
-      let index = articles.indexOf(article);
-      articles.splice(index, 1);
+  Article.findOneAndDelete(deleteBlog, function(err){
+    if (!err) {
       res.redirect("/");
     }
   });
